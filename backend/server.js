e5f5a7af5d610477e4be3411b977b3f7255c8e67
Beatriz Vitória <75254752+biavitoria@users.noreply.github.com>
2025-04-29 02:16:21 -0300

    fix: Ajuste na configuração de CORS

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 app.use(express.json()); // Permite que o Express entenda JSON nas requisições
